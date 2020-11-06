3f2e1123a1452870f42320df59edd55c83fbde54
Logan Klein <lkleindesigns@gmail.com>
2020-11-06 12:26:39 -0600

    Add Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,6 @@
 import { Card } from "react-bootstrap";
+import Rating from './Rating'
+
 
 const Product = ({ product }) => {
   return (
@@ -15,9 +17,7 @@ const Product = ({ product }) => {
         </a>
 
         <Card.Text as='div'>
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating color="gold" value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
 
         <Card.Text as='h3'>
