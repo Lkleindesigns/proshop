de36b62af12eae40f336cfed611e4dd3db51ab17
Logan Klein <lkleindesigns@gmail.com>
2021-01-20 20:17:38 -0600

    Setup payment screen, checkout steps 2 and 3, save payment method and shipping address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,9 +1,14 @@
-import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
+import axios from "axios";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+} from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => {
-  return async ( dispatch, getState ) => {
-    const { data } = await axios.get(`/api/products/${id}`)
+  return async (dispatch, getState) => {
+    const { data } = await axios.get(`/api/products/${id}`);
     dispatch({
       type: CART_ADD_ITEM,
       payload: {
@@ -12,30 +17,47 @@ export const addToCart = (id, qty) => {
         image: data.image,
         price: data.price,
         countInStock: data.countInStock,
-        qty
-      }
-    })
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-  }
-} 
+        qty,
+      },
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(getState().cart.cartItems)
+    );
+  };
+};
 
 export const removeFromCart = (id) => {
   return async (dispatch, getState) => {
     dispatch({
       type: CART_REMOVE_ITEM,
-      payload: id
-    })
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-  }
-} 
+      payload: id,
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(getState().cart.cartItems)
+    );
+  };
+};
 
 export const saveShippingAddress = (data) => {
   return async (dispatch) => {
     dispatch({
       type: CART_SAVE_SHIPPING_ADDRESS,
-      payload: data
-    })
+      payload: data,
+    });
 
-    localStorage.setItem('shippingAddress', JSON.stringify(data))
-  }
-}
+    localStorage.setItem("shippingAddress", JSON.stringify(data));
+  };
+};
+
+export const savePaymentMethod = (data) => {
+  return async (dispatch) => {
+    dispatch({
+      type: CART_SAVE_PAYMENT_METHOD,
+      payload: data,
+    });
+
+    localStorage.setItem("paymentMethod", JSON.stringify(data));
+  };
+};
